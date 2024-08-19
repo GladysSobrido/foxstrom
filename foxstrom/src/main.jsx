25d50d2e4f7b65039ed6034500533153265e7d36
Gladys Sobrido Sambade <gladys@MacBook-Pro-von-Gladys.local>
2024-08-19 11:35:08 +0200

    Button for switching language

---
+++ foxstrom/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+// import i18n (needs to be bundled ;))
+import "./i18n";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
