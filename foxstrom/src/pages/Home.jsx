b6cb67530684d3e192c6f6b07ed7042bc72bf22b
Gladys Sobrido Sambade <gladys@MacBook-Pro-von-Gladys.local>
2024-09-09 15:11:10 +0200

    home content added

--- foxstrom/src/pages/Home.jsx
+++ foxstrom/src/pages/Home.jsx
@@ -74,6 +74,33 @@ function Home() {
           />
         </div>
       </div>
+      {/* End of cards */}
+      <div className="bg">
+        <div className="section2">
+          <h2>
+            Unabhängiger vom Strommarkt: mit dem FoxStrom Kombo und 100 %
+            ÖkoStrom.
+          </h2>
+          <p>
+            Strom selbst produzieren und nutzen, wann immer ihr wollt. Mit dem
+            Kombo von FoxStrom: SolarAnlage, Speicher und Wallbox. Alles aus
+            einer Hand.
+          </p>
+          <h2>Das Solar-Komplettpaket für maximale Ersparnis.</h2>
+          <p>
+            Eine SolarAnlage mit Speicher und smarten Energieservices
+            ermöglichen es euch, Ökostrom aus eurer eigenen PV-Anlage auch dann
+            zu nutzen, wenn die Sonne mal nicht scheint. Frühmorgens, abends und
+            nachts versorgt ihr so eure Elektrogeräte, eine strombetriebene
+            Heizung oder die WallBox für euer E-Auto mit selbsterzeugtem
+            Solarstrom. So könnt ihr eure Stromkosten um bis zu 89 % senken.
+            Zudem entlastet ihr nicht nur euren Geldbeutel, sondern auch das
+            Klima: Mithilfe einer Photovoltaikanlage mit Speicher erzeugt, nutzt
+            und speichert ihr euren Strom direkt vor Ort – ein Transport von
+            dezentral erzeugtem Strom über weite Strecken entfällt.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
