791a3ee65d0986b50d32a1f6e1de45af4c1d9092
Gladys Sobrido Sambade <gladys@MacBook-Pro-von-Gladys.local>
2024-09-09 11:08:37 +0200

    cards not responsive

--- foxstrom/src/pages/Home.jsx
+++ foxstrom/src/pages/Home.jsx
@@ -3,6 +3,9 @@
 import { useTranslation } from "react-i18next";
 import "./Home.css";
 import { Rechner } from "../components/Rechner";
+import { InfoCard } from "../components/InfoCard";
+import { InfoCard2 } from "../components/InfoCard2";
+import infocards from "../data/infocards";
 
 function Home() {
   const { t } = useTranslation();
@@ -16,43 +19,54 @@ function Home() {
           <p>{t("hero_paragraph")}</p>
         </div>
         <div className="rechner">
-          <Rechner
-          // onSubmitData={(verbrauch, anzahl, plz) => {
-          //   setMessage("");
-          //   handleChangeVerbrauch(verbrauch, anzahl);
-          //   handleChangePlz(plz);
-          // }}
-          />
+          <Rechner />
         </div>
       </div>
       {/* These are the cards
       CARD 1:  */}
-      <svg
-        width="600"
-        height="400"
-        viewBox="100 0 300 692"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          id="thePath"
-          d="M 146.5,1.5 C 201.363,0.544049 255.696,5.37738 309.5,16C 343.25,22.7167 374.917,34.7167 404.5,52C 440.851,75.7022 466.017,108.202 480,149.5C 496.982,198.725 505.982,249.392 507,301.5C 508.263,367.532 505.763,433.365 499.5,499C 433.681,506.096 367.681,509.096 301.5,508C 251.024,506.866 202.024,497.866 154.5,481C 93.1113,457.944 51.6113,415.444 30,353.5C 13.6677,304.841 4.66774,254.841 3,203.5C 1.33452,156.72 2.83452,110.053 7.5,63.5C 26.8383,64.4114 46.1716,65.4114 65.5,66.5C 64.4114,46.5044 63.4114,26.5044 62.5,6.5C 90.6193,4.23472 118.619,2.56806 146.5,1.5 Z"
-          //d="M526.636 489.523C598.467 361.569 590.666 251.284 551.879 107.757C538.582 58.5517 506.556 -37.4658 444.069 -103.204C320.276 -233.438 122.218 -189.737 6.51981 -180.688C-109.178 -171.639 -138.103 -67.5724 -164.924 3.12491C-191.745 73.8223 -123.378 416.563 -84.461 503.097L-84.2626 503.538C-45.3885 589.978 0.49324 692 167.445 692C334.682 692 444.781 635.333 526.636 489.523Z"
-          fill="green"
+      <div className="infocards">
+        <InfoCard
+          key={1}
+          title={infocards[0].title}
+          text={infocards[0].text}
+          imgHref={infocards[0].imgHref}
+          imgWidth={infocards[0].imgWidth}
+          height={infocards[0].imgHeight}
+          imgX={infocards[0].imgX}
+          imgY={infocards[0].imgY}
         />
 
-        <clipPath id="cp">
-          <use href="#thePath" />
-        </clipPath>
-
-        <image
-          clipPath="url(#cp)"
-          href="public/stock/pexels-vlada-karpovich-4050415.jpg"
-          width="1000"
-          x="-80"
-          y="-50"
+        <InfoCard
+          key={2}
+          title={infocards[1].title}
+          text={infocards[1].text}
+          imgHref={infocards[1].imgHref}
+          imgWidth={infocards[1].imgWidth}
+          height={infocards[1].imgHeight}
+          imgX={infocards[1].imgX}
+          imgY={infocards[1].imgY}
         />
-      </svg>
+        <InfoCard2
+          key={3}
+          title={infocards[2].title}
+          text={infocards[2].text}
+          imgHref={infocards[2].imgHref}
+          imgWidth={infocards[2].imgWidth}
+          height={infocards[2].imgHeight}
+          imgX={infocards[2].imgX}
+          imgY={infocards[2].imgY}
+        />
+        <InfoCard2
+          key={2}
+          title={infocards[3].title}
+          text={infocards[3].text}
+          imgHref={infocards[3].imgHref}
+          imgWidth={infocards[3].imgWidth}
+          height={infocards[3].imgHeight}
+          imgX={infocards[3].imgX}
+          imgY={infocards[3].imgY}
+        />
+      </div>
     </>
   );
 }
