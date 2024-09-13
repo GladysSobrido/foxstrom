dd87cc0d765ff361dd51bd91e5fb4cb0620fbe78
Gladys Sobrido Sambade <gladys@MacBook-Pro-von-Gladys.local>
2024-09-13 16:09:22 +0200

    private

--- foxstrom/src/pages/private/index.jsx
+++ foxstrom/src/pages/private/index.jsx
@@ -34,64 +34,66 @@ export function PrivatePage() {
     }
     loadProfile();
   }, []);
-  if (customer) {
-    return (
-      <>
-        <header className="personalHeader"></header>
-        <h3>
-          {customer ? (
-            `${(<Trans i18nKey="private1">Willkommen</Trans>)} ${
-              customer.vorname
-            } `
-          ) : (
-            <Spinner />
-          )}
-        </h3>
-        {console.log({ customer })}
-        <div className="section1">
-          <div className="part1">
-            <div className="textgroup">
-              <h4>
-                {" "}
-                <Trans i18nKey="private2">Ihre Persönliche daten:</Trans>
-              </h4>
-              <div className="row">
-                <h5>
-                  {" "}
-                  <Trans i18nKey="private3">Name:</Trans>{" "}
-                </h5>
-                <p className="small">
-                  {customer.vorname} {customer.nachname}
-                </p>
-              </div>
-              <div className="row">
-                <h5>
-                  <Trans i18nKey="private4">Adresse: </Trans>
-                </h5>
-                <p className="small">
-                  {customer.strasse} {customer.hausnummer},
-                </p>
-              </div>
-              <div className="row">
-                <h5>
-                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;{" "}
-                </h5>
-                <p className="small">
-                  {customer.postleitzahl} {customer.ort}
-                </p>
-              </div>
-            </div>
-          </div>{" "}
-          <div className="part2">
-            <TarifResume tarif={customer.tarif} totalPrice={customer.price} />
-          </div>
-          {/* <Link to={"./invoices"}>See your invoices here</Link> */}
-        </div>
-      </>
-    );
-  }
+
   if (!customer) {
     return <Spinner />;
   }
+
+  return (
+    <>
+      <header className="personalHeader"></header>
+      <h3>
+        {customer ? (
+          <>
+            <Trans i18nKey="private1">Willkommen</Trans>
+            {customer.vorname}
+          </>
+        ) : (
+          <Spinner />
+        )}
+      </h3>
+      {console.log({ customer })}
+      <div className="section1">
+        <div className="part1">
+          <div className="textgroup">
+            <h4>
+              {" "}
+              <Trans i18nKey="private2">Ihre Persönliche daten:</Trans>
+            </h4>
+            <div className="row">
+              <h5>
+                {" "}
+                <Trans i18nKey="private3">Name:</Trans>{" "}
+              </h5>
+              <p className="small">
+                {customer.vorname} {customer.nachname}
+              </p>
+            </div>
+            <div className="row">
+              <h5>
+                <Trans i18nKey="private4">Adresse: </Trans>
+              </h5>
+              <p className="small">
+                {customer.strasse} {customer.hausnummer},
+              </p>
+            </div>
+            <div className="row">
+              <h5>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;{" "}
+              </h5>
+              <p className="small">
+                {customer.postleitzahl} {customer.ort}
+              </p>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="part2">
+          <TarifResume tarif={customer.tarif} totalPrice={customer.price} />
+        </div>
+        {/* <Link to={"./invoices"}>See your invoices here</Link> */}
+      </div>
+    </>
+  );
 }
+
 export default PrivatePage;
